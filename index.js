372ec36607e62df68e369ab6dd7bff06138cb806
crandonriordan <krillinmaster@gmail.com>
2018-06-24 11:33:27 -0500

    added press animation for user and computer press

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let redButton = document.getElementById('red');
 let yellowButton = document.getElementById('yellow');
 let blueButton = document.getElementById('blue');
 
+// holds our option of buttons, and other button functions
 let buttonObject = {
   buttons: [greenButton, redButton, yellowButton, blueButton],
   populateButtons: function(numberOfButtons) {
@@ -12,32 +13,84 @@ let buttonObject = {
       let indexOfButtonToAdd = getRandomInt(0, 4);
       buttonArray[i] = this.buttons[indexOfButtonToAdd];
     }
-
     return buttonArray;
   }
 }
 
+
 class Game {
+
   constructor() {
     this.isStarted = false;
     this.isStrict = false;
-    this.gameCount = 0;
-    this.seriesOfButtons = buttonObject.populateButtons(3); //need 20 btns for full simon game
-  }
+    this.playersTurn = false;
+    this.playerGuesses = [];
+    this.numberOfWrongGuesses = 0;
+    this.gameCount = 1; // number of buttons to press (if 20 -> win)
+    this.seriesOfButtons = buttonObject.populateButtons(20); //need 20 btns for full simon game
+  };
 
-  gamePress() {
+  // runs our simulated presses
+  computerPress() {
     // scope issue with this when trying to delay timing
-    let i = 0;
-    let btn = this.seriesOfButtons
-    var interval = setInterval(function () {
-      simulatedPress(this.seriesOfButtons[i]);
-      i++;
+    if(!this.playersTurn && this.isStarted) {
+      let i = 0;
+      let btn = this.seriesOfButtons
+      let interval = setInterval(function () {
+        simulatedPress(this.seriesOfButtons[i]);
+        i++;
 
-      if(i >= this.seriesOfButtons.length) {
-        clearInterval(interval);
-      }
-    }.bind(this), 800);
-  }
+        if(i >= this.gameCount) {
+          clearInterval(interval);
+          this.playersTurn = true;
+          this.playerPress();
+        }
+      }.bind(this), 800);
+    } else {
+      throw `is not players turn or game isn't running`
+    }
+  };
+
+  playerPress() {
+    this.addClickListeners();
+  };
+
+  //helpers
+  addClickListeners() {
+    for(let i = 0; i < buttonObject.buttons.length; i++) {
+      // push the clicked element to playerGuesses property
+      buttonObject.buttons[i].onclick = function() {
+        this.playerGuesses.push(buttonObject.buttons[i]);
+        // check if newly appended playerGuess is correct
+        // if it is not
+        // alert(wrong), removeClickListeners,
+        //playersTurn = false; clear playerGuesses; computerPress();
+        if(this.playerGuesses[this.playerGuesses.length - 1]
+          === this.seriesOfButtons[this.playerGuesses.length - 1]) {
+            if(this.playerGuesses.length === this.gameCount) {
+              this.removeClickListeners();
+              this.playersTurn = false;
+              this.playerGuesses.length = 0;
+              this.gameCount++;
+              this.computerPress();
+            }
+        } else {
+            alert("wrong guess");
+            this.removeClickListeners();
+            this.playersTurn = false;
+            this.playerGuesses.length = 0;
+            this.computerPress();
+        }
+      }.bind(this);
+    }
+  };
+
+  removeClickListeners() {
+    for(let i = 0; i < buttonObject.buttons.length; i++) {
+      buttonObject.buttons[i].onclick = null;
+    }
+  };
+
 }
 
 // helper functions
@@ -49,7 +102,7 @@ function getRandomInt(min, max) {
 }
 
 // Start up
-let game = new Game();
+
 
 function simulatedPress(button) {
   playSound(button.id);
@@ -63,3 +116,7 @@ function playSound(color) {
   sounds[color].load();
   sounds[color].play();
 }
+
+
+let game = new Game();
+game.isStarted = true;
